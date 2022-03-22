6688535a9e50224c8edffec684b9f581f4fef172
Valeeswaran <valeeswaran.k@gmail.com>
2022-03-22 23:50:01 +0800

    Updating after completing the functional way of programming course. All the files are practiced by myself.

---
+++ src/functional/functional1.py
@@ -0,0 +1,15 @@
+ENVIRONMENT = "dev"
+
+def fetch_data_real():
+    print("fetching data from the databse")
+
+def fetch_data_fake():
+    print("fetching fake data")
+    return {
+        "name": "Valee",
+        "age": 40
+    }
+
+fetch_data = fetch_data_real if ENVIRONMENT == "prod" else fetch_data_fake
+data = fetch_data()
+print(data)
